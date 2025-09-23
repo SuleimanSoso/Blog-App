a5eace240b2a377eb270de4c77c82b6a84572b19
SuleimanSoso <nadraadimu402@gmail.com>
2025-09-23 21:46:42 +0300

    Use dynamic PORT for Render deployment

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ const server = http.createServer((req, res) => {
 });
 
 
-server.listen(3000, '0.0.0.0', () => {
-    console.log('listening for requests on port 3000')
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
